5b0f4b3dfc5cf782a0384eb3f6bf16ed6f81ad4b
Phạm Đăng Đông <110766567+Anodis108@users.noreply.github.com>
2024-11-05 10:06:22 +0700

    fix: rechange the architechture

---
+++ src/data_argumentation/__init__.py
@@ -0,0 +1,2 @@
+from common.read_save_img import read_img, save_img
+from common.config import IMAGE_PATH, IMAGE_PATH2, SAVE_IMAGE_PATH
---
+++ src/data_argumentation/geometric_transformations/__init__.py
@@ -0,0 +1,2 @@
+from data_argumentation import read_img, save_img
+from data_argumentation import IMAGE_PATH, IMAGE_PATH2, SAVE_IMAGE_PATH
